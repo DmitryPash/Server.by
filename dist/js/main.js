ffda0adab0aaf7340208698e4f281fde11ac49f9
DmitryPashkou <86959216+DmitryPash@users.noreply.github.com>
2021-12-07 12:32:21 +0100

    new change
    new change

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,11 +1,11 @@
 $(document).ready(function () {
-  $(".middle-banner-slider").slick({
+  $(".banner-slider").slick({
     dots: true,
   });
 });
 
 $(document).ready(function () {
-  $(".middle-offer-slider").slick({
+  $(".offer-slider").slick({
     slidesToShow: 5,
     infinite: true,
     prevArrow: ".slider-offer-left",
@@ -51,47 +51,33 @@ $(document).ready(function () {
 });
 
 $(document).ready(function () {
-  $(".middle-hits-slider").slick({
+  $(".hits-slider").slick({
     slidesToShow: 5,
     infinite: true,
     prevArrow: ".slider-hits-left",
     nextArrow: ".slider-hits-right",
     variableWidth: true,
-    // responsive: [
-    //   {
-    //     breakpoint: 1271,
-    //     settings: {
-    //       slidesToShow: 4,
-    //     },
-    //   },
-    //   {
-    //     breakpoint: 971,
-    //     settings: {
-    //       slidesToShow: 3,
-    //     },
-    //   },
-    //   {
-    //     breakpoint: 771,
-    //     settings: {
-    //       slidesToShow: 2,
-    //     },
-    //   },
-    //   {
-    //     breakpoint: 576,
-    //     settings: {
-    //       slidesToShow: 2,
-    //       prevArrow: false,
-    //       nextArrow: false,
-    //     },
-    //   },
-    //   {
-    //     breakpoint: 500,
-    //     settings: {
-    //       slidesToShow: 1,
-    //       prevArrow: false,
-    //       nextArrow: false,
-    //     },
-    //   },
-    // ],
+  });
+});
+
+// SLider page-product.html
+
+$(document).ready(function () {
+  $(".slider-for").slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    arrows: false,
+    fade: true,
+    dots: false,
+    asNavFor: ".slider-nav",
+  });
+  $(".slider-nav").slick({
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    asNavFor: ".slider-for",
+    dots: false,
+    arrows: true,
+    // centerMode: true,
+    focusOnSelect: true,
   });
 });
